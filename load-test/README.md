8c0d02cc9bf673000fbf48da2411393cc9b5954f
gogokotsev00 <Kotsev75@gmail.com>
2023-06-17 22:36:50 +0200

    Updated locust test and readme

--- load-test/README.md
+++ load-test/README.md
@@ -4,7 +4,7 @@ These tests simulate actual end user usage of the application. They are used to 
 
 ### Parameters
 * `[host]` - The hostname (and port if applicable) where the application is exposed. (Required)
-* `[number of clients]` - Peak number of concurrent Locust users. (Optional: Default is 2)
+* `[number of concurrent clients]` - Peak number of concurrent Locust users. (Optional: Default is 2)
 * `[number of iterations]` - The total number of iterations of the load test. (Optional: Default is 10)
 * `[spawn rate]` - Specifies the rate at which new simulated users are spawned per second during a load test. Increases until the peak number of concurrent users is reached. (Optional: Default is 1)
 
@@ -13,9 +13,17 @@ These tests simulate actual end user usage of the application. They are used to 
 ### Requirements 
 * [locust](https://github.com/locustio/locust) `pip install locust`
 * [locust-plugins](https://github.com/SvenskaSpel/locust-plugins) `pip install locust-plugins`
-* Start command `./runLocust.sh -h [host] -u [number of clients] -r [spawn rate] -i [number of iterations]`
+* Start command `./runLocust.sh -h [host] -u [number of concurrent clients] -r [spawn rate per seconds] -i [number of iterations]`
 
 
 ## Running in Docker Container
 * Build `docker build -t load-test .`
 * Run `docker run load-test -h [host] -c [number of clients] -r [number of requests]`
+
+## Helpful information regarding our test
+1. At the beginning a user object is initiated with random username, password, email, card details and address details
+2. When the object is started we use the `on_start` method to define some steps in our setup preparation like:
+    * user registration
+    * card and address creation
+3. Afterwards `Locust` starts executing the marked with `@task` methods. If we have more than one we can also add weight like `task(NUMBER_FROM_1_TO_5)` which will indicate that the task with the higher weight should be executed first. In these tasks we can create whatever scenario we need using HTTP requests as can be seen in the `locustfile.py`.
+4. At the end we have an `on_stop` method which is called when the test reach its end where we delete the created test users.
--- load-test/locustfile.py
+++ load-test/locustfile.py
@@ -4,53 +4,49 @@ import secrets
 import string
 
 from locust import HttpUser, TaskSet, task
-from random import randint, choice
+from random import choice
 import locust_plugins
 
 class WebTasks(TaskSet):
-    # the __init__ method is called, when an object is created from the class and initializes various attributes of the object (self)
+##### CONSTANTS #####
+    NUMBERS = 1
+    LETTERS = 2
+
+##### INITIATION OF OBJECT #####
+
+    # The __init__ method is called, when an object is created from the class and initializes various attributes of the object (self)
     def __init__(self, parent):
         super().__init__(parent)
-        self.username = "user19"     #  TODO create a user with a random name every time, so that the load test is better scalable
+        self.user_id = ''
+        self.username = self.generate_random_username()
         self.password = "password"
         self.email = self.username + "@test.com"
         self.card_details = self.generate_random_card()
         self.address_details = self.generate_random_address()
 
-    # Generate random card in the according format using the helper function
+    # Generates random card in the according format using the helper function
     def generate_random_card(self):
         card = {
-            "longNum": generate_random_id(16, "numbers"),
-            "expires": generate_random_id(2, "numbers") + "/" + generate_random_id(2, "numbers"),
-            "ccv": generate_random_id(3, "numbers"),
+            "longNum": generate_random_id(16, self.NUMBERS),
+            "expires": generate_random_id(2, self.NUMBERS) + "/" + generate_random_id(2, self.NUMBERS),
+            "ccv": generate_random_id(3, self.NUMBERS),
         }
         return card
 
-    # Generate random address in the according format using the helper function
+    # Generates random address in the according format using the helper function
     def generate_random_address(self):
         address = {
-            "street": generate_random_id(10, "letters"),
-            "number": generate_random_id(3, "numbers"),
-            "country": generate_random_id(8, "letters"),
-            "city": generate_random_id(6, "letters"),
-            "postcode": generate_random_id(5, "numbers"),
+            "street": generate_random_id(10, self.LETTERS),
+            "number": generate_random_id(3, self.NUMBERS),
+            "country": generate_random_id(8, self.LETTERS),
+            "city": generate_random_id(6, self.LETTERS),
+            "postcode": generate_random_id(5, self.NUMBERS),
         }
         return address
 
-    # This method is called when a new user (virtual user) starts running
-    def on_start(self):
-        userID = self.register()
-        self.create_card()
-        self.create_address()
-
-        # Add the userID as a property of the card and address
-        self.card_details.update({"userID" : userID})
-        self.address_details.update({"userID" : userID})
-        
-        cleanup(self, self.card_details.get("userID"))
-        #print(self.card_details)
-        #print(self.address_details)
-
+    # Generates random username for each user
+    def generate_random_username(self):
+        return 'user{}'.format(generate_random_id(4, self.NUMBERS))
 
     # Registers the user
     def register(self):
@@ -64,7 +60,6 @@ class WebTasks(TaskSet):
         response_json = response.json()
         id = response_json.get("id")
         return id
-        
 
     # Creates a card for the user
     def create_card(self):
@@ -74,10 +69,9 @@ class WebTasks(TaskSet):
             "ccv": self.card_details["ccv"],
         }
         response = self.client.post("/cards", json=card_payload)
-        response_json = response.json()
-        id = response_json.get("id")
+        response_json = response.json()  # From here to is only for test purposes
+        id = response_json['id']
         print("Card ID:", id)
-        
 
     # Adds an address to the current user
     def create_address(self):
@@ -89,15 +83,27 @@ class WebTasks(TaskSet):
             "postcode": self.address_details["postcode"],
         }
         response = self.client.post("/addresses", json=address_payload)
-        response_json = response.json()
-        id = response_json.get("id")
+        response_json = response.json()  # From here to is only for test purposes
+        id = response_json['id']
         print("Address ID:", id)
 
+##### SCENARIO FUNCTIONS #####
+
+    # This method is called when a new user (virtual user) starts running
+    def on_start(self):
+        self.user_id = self.register()
+        print('Username: {}'.format(self.username))
+        
+        self.create_card()
+        self.create_address()
+
+        # Add the userID as a property of the card and address
+        self.card_details.update({"userID" : self.user_id})
+        self.address_details.update({"userID" : self.user_id})
 
     # Defines the user's behavior or actions that are repeated during load testing or performance testing.
     @task
     def load(self):
-
         catalogue = self.client.get('/catalogue').json()
         category_item1 = choice(catalogue)
         item_id1 = category_item1['id']
@@ -109,7 +115,7 @@ class WebTasks(TaskSet):
         item_id3 = category_item3['id']
 
         self.client.get('/')
-        #self.client.get("/login", auth=(self.username, self.password))
+        self.client.get("/login", auth=(self.username, self.password))
 
         self.client.get('/category.html')
         self.client.get('/detail.html?id={}'.format(item_id1))
@@ -121,25 +127,26 @@ class WebTasks(TaskSet):
         self.client.get('/detail.html?id={}'.format(item_id1))
         self.client.post('/cart', json={'id': item_id3, 'quantity': 2})
 
-        # Commented these 2 lines out, because I wanted to only test the creation of a user with a card and an address, feel free to comment back in
-        #self.client.get('/basket.html')
-        #self.client.post('/orders')
+        self.client.get('/basket.html')
+        self.client.post('/orders')
 
-# Helper function that generates random IDs (either numbers only, letters only)
+    # This method is called when a new user (virtual user) stops running
+    def on_stop(self):
+        self.client.delete('/customers/{}'.format(self.user_id))
+
+##### HELPER FUNCTIONS #####
+
+# Generates random IDs (either numbers only, letters only or hexadecimal string)
 def generate_random_id(length, output_type):
-    if output_type == "numbers":
+    if output_type == 1:
         characters = string.digits
-    elif output_type == "letters":
+    elif output_type == 2:
         characters = string.ascii_letters
     else:
         characters = string.hexdigits[:-6]  # Exclude letters a-f for mixed hex string
-    
+
     random_id = ''.join(secrets.choice(characters) for _ in range(length))
     return random_id
-
-# Deletes the users with the respective Id
-def cleanup(self, user_id):
-    self.client.delete('/customers/{}'.format(user_id))
 
 class Web(HttpUser):
     tasks = [WebTasks]
