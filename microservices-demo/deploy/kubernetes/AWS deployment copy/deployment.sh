9ddd95c8102f9870ea54e75e66e769956d12594c
krutarth4 <74930393+krutarth4@users.noreply.github.com>
2023-08-25 18:38:13 +0200

    deployment tested

--- microservices-demo/deploy/kubernetes/AWS deployment copy/deployment.sh
+++ microservices-demo/deploy/kubernetes/AWS deployment copy/deployment.sh
@@ -22,6 +22,9 @@ kubectl apply -f /tmp/Complete_parts/complete6.yaml
 sleep 20
 kubectl get nodes && kubectl get pods -n sock-shop
 sleep 3
+kubectl apply -f /tmp/Complete_parts/complete7.yaml
+sleep 20
 
 echo "Deployment complete showing results"
 kubectl get nodes && kubectl get pods -A
+echo "Deployment complete !!!!!"
